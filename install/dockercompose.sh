cec8dd3982be4e2ae10c66d0255683fb946a0c62
Miflop <miguel@miflop.com>
2018-05-25 21:51:32 +0200

    Components

--- install/dockercompose.sh
+++ install/dockercompose.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-printf "{Red}Installing Docker Compose 1.21.2"
 sudo curl -L https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose >/dev/null
 sudo chmod +x /usr/local/bin/docker-compose >/dev/null
 docker-compose --version
